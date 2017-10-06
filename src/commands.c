f91b74b55810c5b5f895286f52b9412af7520765
Lee SungHun <gldiwkd@ajou.ac.kr>
2017-10-06 23:28:58 +0900

    all test passed

--- src/commands.c
+++ src/commands.c
@@ -1,30 +1,58 @@
 #include <string.h>
-
+#include <stdio.h>
 #include "commands.h"
-
-int do_cd(int argc, char** argv) {
-  if (!validate_cd_argv(argc, argv))
-    return -1;
-
-  // TODO: Fill it!
-  return 0;
+#include <unistd.h>
+int do_cd(int argc, char** argv) 
+{
+	if (!validate_cd_argv(argc, argv))
+		return -1;
+	
+	chdir(argv[1]);
+	return 0;
 }
 
-int do_pwd(int argc, char** argv) {
-  if (!validate_pwd_argv(argc, argv))
-    return -1;
+int do_pwd(int argc, char** argv) 
+{
+	if (!validate_pwd_argv(argc, argv))
+   		 return -1;
 
-  // TODO: Fill it!
+	else
+	{
+                char buf[2048];
+                getcwd(buf,8096);
+                printf("%s\n",buf);
+                return 0;
 
-  return 0;
+        }
+
 }
 
-int validate_cd_argv(int argc, char** argv) {
-  // TODO: Fill it!
-  return 1;
+
+
+int validate_cd_argv(int argc, char** argv)
+{
+        if(argc!=2)
+        {
+                return 0;
+
+        }
+	else if(strcmp(argv[0],"cd"))
+	{
+		return 0;
+	}
+        else
+        {
+                return 1;
+        }
 }
 
-int validate_pwd_argv(int argc, char** argv) {
-  // TODO: Fill it!
-  return 1;
+int validate_pwd_argv(int argc, char** argv)
+{
+
+
+	int check = strcmp(argv[0],"pwd");
+
+	if((!check)&&(argc==1))
+		return 1;
+	return 0;
 }
--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,48 @@
 #include "utils.h"
-
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
+
 {
-  // TODO: Fill this!
+	*argc = 0;
+	*argv = (char**)malloc(sizeof(char*)*10);
+	for(int i=0; i<10; i++)
+	{
+		(*argv)[i] = (char*)malloc(sizeof(char)*10);
+	}
+
+	char copy[8096];
+	strcpy(copy, command);
+
+		char* result;
+		result = strtok(copy," \t\n");
+		while(result!=NULL)
+		{
+			strcpy((*argv)[*argc],result);
+
+			*argc = *argc+1;
+			result = strtok(NULL," \t\n");
+		}
+
+	if((*argc)==0)
+	{
+		strcpy((*argv)[0], "");
+		(*argc) = 1;
+	}
+
+	/*
+
+	int j;
+	for(j=0; j<10; j++)
+	{
+		free((*argv)[j]);
+	}
+
+	free(*argv);
+        */
+		
+		
+	
 }
